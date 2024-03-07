71781f6ecb25a2c2bfebda055b9551042edd1e05
Amit <amity2001@gmail.com>
2024-03-07 11:22:23 +0530

    Added new controls on test form

--- HelloWinforms/Form1.Designer.cs
+++ HelloWinforms/Form1.Designer.cs
@@ -29,13 +29,47 @@ namespace HelloWinforms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.monthCalendarx1 = new HelloWinforms.MonthCalendarX();
+            this.datePicker1 = new HelloWinforms.DatePicker();
+            this.SuspendLayout();
+            // 
+            // monthCalendarx1
+            // 
+            this.monthCalendarx1.Font = new System.Drawing.Font("Courier New", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monthCalendarx1.Location = new System.Drawing.Point(11, 11);
+            this.monthCalendarx1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.monthCalendarx1.Name = "monthCalendarx1";
+            this.monthCalendarx1.Size = new System.Drawing.Size(271, 276);
+            this.monthCalendarx1.TabIndex = 0;
+            // 
+            // datePicker1
+            // 
+            this.datePicker1.AnchorSize = new System.Drawing.Size(121, 21);
+            this.datePicker1.BackColor = System.Drawing.Color.White;
+            this.datePicker1.DockSide = HelloWinforms.DropDownControl.eDockSide.Left;
+            this.datePicker1.Location = new System.Drawing.Point(471, 104);
+            this.datePicker1.Name = "datePicker1";
+            this.datePicker1.Size = new System.Drawing.Size(121, 21);
+            this.datePicker1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.datePicker1);
+            this.Controls.Add(this.monthCalendarx1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private MonthCalendarX monthCalendarx1;
+        private DatePicker datePicker1;
     }
 }
 
