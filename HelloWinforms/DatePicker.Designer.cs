0cf26c4720046b99620f268d89693d3120506ba2
Amit <amity2001@gmail.com>
2024-03-07 11:17:46 +0530

    Added custom DatePicker and DropDown controls

---
+++ HelloWinforms/DatePicker.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace HelloWinforms
+{
+    partial class DatePicker
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DatePicker));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.monthCalendarx1 = new HelloWinforms.MonthCalendarX();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.monthCalendarx1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // monthCalendarx1
+            // 
+            this.monthCalendarx1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.monthCalendarx1.Font = new System.Drawing.Font("Courier New", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.monthCalendarx1.Location = new System.Drawing.Point(0, 0);
+            this.monthCalendarx1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.monthCalendarx1.Name = "monthCalendarx1";
+            this.monthCalendarx1.Size = new System.Drawing.Size(150, 150);
+            this.monthCalendarx1.TabIndex = 0;
+            this.monthCalendarx1.DateChanged += new System.EventHandler(this.monthCalendarx1_DateChanged);
+            // 
+            // DatePicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Name = "DatePicker";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MonthCalendarX monthCalendarx1;
+    }
+}
---
+++ HelloWinforms/DatePicker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWinforms
+{
+    public partial class DatePicker : DropDownControl
+    {
+        private bool isUpdating;
+        public DatePicker()
+        {
+            InitializeComponent();
+            InitializeDropDown(panel1);
+            isUpdating = false;
+        }
+        public event EventHandler ProjectStageChanged;
+
+
+        public string Stage
+        {
+            get { return ""; }
+        }
+
+        private void monthCalendarx1_DateChanged(object sender, EventArgs e)
+        {
+            if (isUpdating || this.DropState != eDropState.Dropped) return;
+
+            this.CloseDropDown();
+            //this.Text = this.Stage + " Stage";
+            //if (ProjectStageChanged != null)
+            //    ProjectStageChanged(null, null);
+        }
+    }
+}
