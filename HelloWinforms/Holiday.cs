f2103163c3d73585bbd621af37e1e9e9d80f5bc4
Amit <amity2001@gmail.com>
2024-03-18 11:07:37 +0530

    Renamed calendar enum to CalendarType and added new class Calendar

--- HelloWinforms/Holiday.cs
+++ HelloWinforms/Holiday.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,19 @@ namespace HelloWinforms
         }
     }
 
-    public enum Calendar
+    public class Calendar
+    {
+        public CalendarType CalendarType { get; set; }
+        public Color Color { get; set; }
+
+        public Calendar( CalendarType calendarType, Color color)
+        {
+            Color = color;
+            CalendarType = calendarType;
+        }
+    }
+
+    public enum CalendarType
     {
         Gregorian,
         Indian,
